44d301cbd34f37d0d3f8eb0e3adef0ce318f4f4d
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-04-07 23:55:14 -0500

    Add tests to components/Nav

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// previously was: 
+// import ReactDOM from 'react-dom';
+// now is:
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// previously was:
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+// now is:
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
